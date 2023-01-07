2d65fe4d4eb3e02b989e851b9577a1ab42ca5626
karthikmurakonda <karthik.murakonda14@gmail.com>
2023-01-07 23:55:25 +0530

    Corrected title in App bar

--- lib/main.dart
+++ lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Hacker News'),
     );
   }
 }
